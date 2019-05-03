ed36aa2b5795d2c6d6e4f0d15373df21af24b7ae
CauaRebelo <cauarebelo@discente.ufg.br>
2019-05-02 21:21:58 -0300

    Documentação Parte 2

--- aula-01/js/ex3.js
+++ aula-01/js/ex3.js
@@ -1,3 +1,15 @@
+/**
+ * Informa o dia da semana, sendo 0 segunda e assim por diante
+ * 
+ * @param {number} dia Dia desejado
+ * @param {number} mes O mes desejado
+ * @param {number} ano O ano desejado
+ * 
+ * @returns {number} Retorna o dia da semana, sendo 0 segunda ate 6 como domingo
+ * 
+ * @throws {RangeError} Se o o dia for menor a 1 ou maior que 31, ou se o mes for menor que 1 ou maior que 12
+ * ou se o ano for menor que 1753
+ */
 function diadasemana(dia, mes, ano) {
     if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano <= 1753) {
         throw new RangeError("Numero fora da faixa esperada");
