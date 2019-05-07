14c8614518fa5a1102038fb603737378bd513b52
CauaRebelo <cauarebelo@discente.ufg.br>
2019-05-06 23:13:22 -0300

    Mais testes

---
+++ aula-01/js/teste/Fatorial.test.js
@@ -0,0 +1,25 @@
+const funcao = require("../ex6");
+
+test("Fatorial de 5 = 120 (caso classico)", () => {
+    expect(funcao(5)).toBe(120);
+});
+
+
+test("argumento null/undefined", () => {
+    expect(() => { funcao(); }).toThrow();
+});
+
+
+test("argumento nao numerico", () => {
+    expect(() => { funcao("abcd"); }).toThrow();
+});
+
+
+test("valor negativo fora da faixa", () => {
+    expect(() => { funcao(-1); }).toThrow(RangeError);
+});
+
+
+test("valor deve ser inteiro", () => {
+    expect(() => funcao(10.3)).toThrow(RangeError);
+});
