8cd33967d5e354bbb11d7a44e56bdf2f8d613354
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-06-25 16:03:40 -0300

    FIXME

--- aula-01/js/raiz.js
+++ aula-01/js/raiz.js
@@ -13,18 +13,32 @@
  * @throws {RangeError} Se n ou i forem menor que 0
  */
 function raiz(n , i) {
+    // FIXME condição complexa coloque em função própria
     if (n == null || n == undefined || i == null || i == undefined) {
         throw new Error("Entrada invalidada, null e undefined nao sao entradas validas");
     }
+    
     if (typeof(n) !== "number" || typeof(i) !== "number") {
         throw new TypeError("Entrada nao e um numero");
     }
+    
     if (Math.trunc(n) != n || Math.trunc(i) != i) {
         throw new RangeError("Numero nao e inteiro");
     }
+    
     if (n < 0 || i < 0) {
         throw new RangeError("Numero fora da faixa esperada");
     }
+    // FIXME observe que o código acima é específico para verificação dos argumentos
+    // e maior que o código "útil", fornecido abaixo. Caso clássico de se colocar
+    // em função própria. A função a ser criada pode ser local à presente função raiz. 
+    // Ou seja, neste caso, teremos uma função na qual são definidas duas outras, uma 
+    // para a condição e outra que reúne várias linhas, inclusive a condição.
+    
+    // FIXME Temos três FIXME neste arquivo. Observe que a variável fornecida como
+    // argumento é utilizada como variável da função. Este não é um uso recomendado. 
+    // Parâmetros devem ser tratados como constantes. Ou seja, copie o valor de i para 
+    // a variável que você irá usar nesta função. 
     let r = 1;
     while (i >= 0) {
         r = (r + n/r) / 2;
